4531be11a422efdd5ff9cb8480e486ad0a0521be
ljxun <591692130@qq.com>
2025-03-01 18:55:07 +0800

    开机启动服务和任务脚本

---
+++ start_script.bat
@@ -0,0 +1,11 @@
+
+
+
+start D:\OnmyojiAutoScript\ljxun\toolkit\pythonw.exe "D:\OnmyojiAutoScript\ljxun\start_script.py"  DU
+start D:\OnmyojiAutoScript\ljxun\toolkit\pythonw.exe "D:\OnmyojiAutoScript\ljxun\start_script.py"  MI
+
+
+
+
+
+
