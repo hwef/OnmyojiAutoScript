e02ce4e47feec320a3791c9cd48c5ea0c16c5a18
ljxun <591692130@qq.com>
2025-03-23 10:12:04 +0800

    refactor(tasks): 注释掉 Dokan 配置项
    - 注释掉了多个 Dokan 配置项，包括攻击优先顺序、自动加油、随机延迟和防封设置

--- tasks/Dokan/config.py
+++ tasks/Dokan/config.py
@@ -20,16 +20,16 @@ class DokanConfig(BaseModel):
     # dokan_declear_war_priority: int = Field(default=0, description='dokan_declear_war_priority_help')
 
     # 攻击优先顺序: 见习=0,初级=1...
-    dokan_attack_priority: int = Field(default=0, description='dokan_attack_priority_help')
+    # dokan_attack_priority: int = Field(default=0, description='dokan_attack_priority_help')
 
     # 失败CD后自动加油
-    dokan_auto_cheering_while_cd: bool = Field(default=False, description='dokan_auto_cheering_while_cd_help')
+    # dokan_auto_cheering_while_cd: bool = Field(default=False, description='dokan_auto_cheering_while_cd_help')
 
     # 正式进攻会设定 2s - 10s 的随机延迟，避免攻击间隔及其相近被检测为脚本。
-    random_delay: bool = Field(default=False, description='random_delay_help')
+    # random_delay: bool = Field(default=False, description='random_delay_help')
 
     # 防封：使用固定的随机区域进行随机点击，若为False将自动识别当前画面中的最大纯色区域作为随机点击区域
-    anti_detect_click_fixed_random_area: bool = Field(default=False, description='anti_detect_click_fixed_random_area_help')
+    # anti_detect_click_fixed_random_area: bool = Field(default=False, description='anti_detect_click_fixed_random_area_help')
 
 class Dokan(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
