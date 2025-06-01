641b2ae469e5fbb9db2809905ed4ad4763d69757
hwef <2643713188@qq.com>
2025-06-01 14:37:28 +0800

    降低花合战识别阈值

--- tasks/TalismanPass/script_task.py
+++ tasks/TalismanPass/script_task.py
@@ -100,7 +100,7 @@ class ScriptTask(GameUi, TalismanPassAssets):
 if __name__ == '__main__':
     from module.config.config import Config
     from module.device.device import Device
-    c = Config('oas1')
+    c = Config('oas2')
     d = Device(c)
     t = ScriptTask(c, d)
     t.screenshot()
