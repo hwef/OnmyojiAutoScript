f51d3668701e4e0ddda68079242154cc93aadce3
ljxun <591692130@qq.com>
2025-06-10 09:51:36 +0800

    refactor(TrueOrochi):优化真·八岐大蛇任务结束逻辑
    - 在未发现真蛇时，增加推送通知并设置任务完成状态
    - 在任务成功完成时，简化通知内容并优化下次运行时间设置
    - 增加任务下周执行通知

--- tasks/TrueOrochi/script_task.py
+++ tasks/TrueOrochi/script_task.py
@@ -53,7 +53,8 @@ class ScriptTask(OrochiScriptTask, TrueOrochiAssets):
             # 判断是否需要挑战十层触发真蛇
             if not conf.find_true_orochi:
                 logger.info('Not find_true_orochi_help')
-                self.check_times(False)
+                self.config.notifier.push(title='真·八岐大蛇', content=f'未发现真蛇，本周已完成{conf.current_success}次')
+                self.set_next_run('TrueOrochi', finish=True, success=True)
                 raise TaskEnd('TrueOrochi')
 
             self.check_layer(Layer.TEN)
@@ -83,7 +84,8 @@ class ScriptTask(OrochiScriptTask, TrueOrochiAssets):
 
         if not battle:
             # 如果还没有真蛇，那么就退出
-            self.check_times(battle)
+            self.config.notifier.push(title='真·八岐大蛇', content=f'未发现真蛇，本周已完成{conf.current_success}次')
+            self.set_next_run('TrueOrochi', finish=True, success=True)
             raise TaskEnd('TrueOrochi')
         # 如果有真蛇，那么就开始战斗
         logger.hr('True Orochi Battle')
@@ -177,7 +179,7 @@ class ScriptTask(OrochiScriptTask, TrueOrochiAssets):
         :return:
         """
         conf = self.config.true_orochi.true_orochi_config
-        self.config.notifier.push(title='真·八岐大蛇', content=f'本周已完成{conf.current_success}次，请查看截图')
+        self.config.notifier.push(title='真·八岐大蛇', content=f'本周已完成{conf.current_success}次')
 
         # 超过两次就说明这周打完了,设置下次运行时间为下周一，次数重置为0
         if conf.current_success >= 2:
--- tasks/base_task.py
+++ tasks/base_task.py
@@ -528,6 +528,8 @@ class BaseTask(GlobalGameAssets, CostumeBase):
         scheduler = getattr(task_object, 'scheduler', None)
         server_update = scheduler.server_update
 
+        self.config.notifier.push(title=I18n.trans_zh_cn(TaskName), content=f'任务下周{target_day}执行')
+
         # 调用自定义函数设置下一次运行时间
         self.custom_next_run(task=TaskName,
                              custom_time=Time(hour=server_update.hour, minute=server_update.minute,
