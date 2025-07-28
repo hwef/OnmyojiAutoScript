077928f59c517ceb04d49dc6f51ef6e29f1c7ccf
ljxun <591692130@qq.com>
2025-07-28 10:22:46 +0800

    refactor(RichMan): 优化剩余购买次数的正则匹配逻辑
    - 增加对 ':' 的检查，以处理不同格式的结果
    - 更新正则表达式以匹配两种分隔符

--- tasks/RichMan/mall/special.py
+++ tasks/RichMan/mall/special.py
@@ -165,8 +165,8 @@ class Special(Buy, MallNavbar):
         result = result.replace('？', '2').replace('?', '2').replace(':', '；').replace('火', '次').replace('教', '数')
         result = result.replace('刺', '剩').replace('利', '剩')
         try:
-            if '：' in result:
-                result = re.findall(r'剩余购买次数：(\d+)', result)[0]
+            if '：' in result or ':' in result:
+                result = re.findall(r'剩余购买次数[:：](\d+)', result)[0]
                 result = int(result)
             else:
                 result = re.findall(r'本周剩余数量(\d+)', result)[0]
