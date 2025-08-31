5f401aa975be539a759c15244b338f89a8f0f788
hwef <2643713188@qq.com>
2025-08-31 09:20:03 +0800

    chore: 更新.gitignore并调整测试配置
    更新.gitignore文件，添加本地测试文件和文件夹
    将KekkaiUtilize任务配置从'oas1'改为'oas2'
    修改RichMan/mall/medal.py中的匹配方法

--- tasks/KekkaiUtilize/script_task.py
+++ tasks/KekkaiUtilize/script_task.py
@@ -722,7 +722,7 @@ if __name__ == "__main__":
     from module.config.config import Config
     from module.device.device import Device
 
-    c = Config('oas1')
+    c = Config('oas2')
     d = Device(c)
     t = ScriptTask(c, d)
     t.run()
