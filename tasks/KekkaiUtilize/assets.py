f485256f7fe9c225e55467fa835b5d246e15a078
ljxun <591692130@qq.com>
2025-09-02 16:17:38 +0800

    perf(KekkaiUtilize): 蹭卡固定Y轴坐标，以便于准确上下滑动距离

--- tasks/KekkaiUtilize/assets.py
+++ tasks/KekkaiUtilize/assets.py
@@ -153,7 +153,7 @@ class KekkaiUtilizeAssets:
 
 	# Swipe Rule Assets
 	# 向下滑动 
-	S_U_UP = RuleSwipe(roi_front=(247,520,43,20), roi_back=(247,180,43,20), mode="default", name="u_up")
+	S_U_UP = RuleSwipe(roi_front=(247,520,100,1), roi_back=(247,104,100,1), mode="default", name="u_up")
 	# 滑动到底部 
 	S_U_END = RuleSwipe(roi_front=(175,179,26,26), roi_back=(164,518,49,102), mode="default", name="u_end")
 
