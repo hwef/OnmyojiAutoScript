75187e59ae14cfef894c3feeb17d00e9747c4d93
ljxun <591692130@qq.com>
2025-09-05 23:59:15 +0800

    refactor(tasks): 重构 wait_until_appear_then_click 函数
    - 移除不必要的 action 参数，改为使用 wait_time 参数
    - 优化函数逻辑，增加返回值以指示

--- tasks/base_task.py
+++ tasks/base_task.py
@@ -217,20 +217,19 @@ class BaseTask(GlobalGameAssets, CostumeBase):
 
     def wait_until_appear_then_click(self,
                                      target: RuleImage,
-                                     action: Union[RuleClick, RuleLongClick] = None) -> None:
+                                     wait_time: int = None) -> bool:
         """
         等待直到出现目标，然后点击
-        :param action:
         :param target:
+        :param wait_time:
         :return:
         """
-        self.wait_until_appear(target)
-        if action is None:
-            self.device.click(target.coord(), control_name=target.name)
-        elif isinstance(action, RuleLongClick):
-            self.device.long_click(target.coord(), duration=action.duration / 1000, control_name=target.name)
-        elif isinstance(action, RuleClick):
-            self.device.click(target.coord(), control_name=target.name)
+        if self.wait_until_appear(target, wait_time=wait_time):
+            x, y = target.coord()
+            self.device.click(x=x, y=y, control_name=target.name)
+            return True
+        else:
+            return False
 
     def wait_until_disappear(self, target: RuleImage) -> None:
         while 1:
