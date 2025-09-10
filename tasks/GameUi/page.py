e2bf27a81aa19a2a6cbbfebe33071b4e757a0f1e
ljxun <591692130@qq.com>
2025-09-10 20:28:45 +0800

    refactor(GameUi): 调整狩猎战标志页
    - 修改 page_img_2.json 和 assets.py 中的 I_CHECK_HUNT 图像规则
    - 更新 page.py 中的 page_hunt 页面链接，使用 G.I_BACK_YOLLOW 替代 G.I_BACK_BL

--- tasks/GameUi/page.py
+++ tasks/GameUi/page.py
@@ -108,7 +108,7 @@ page_demon_encounter.link(button=G.I_DEMON_ENCOUNTER_GOTO_TOWN, destination=page
 page_town.link(button=G.I_TOWN_GOTO_DEMON_ENCOUNTER, destination=page_demon_encounter)
 # 狩猎战 hunt
 page_hunt = Page(G.I_CHECK_HUNT)
-page_hunt.link(button=G.I_BACK_BL, destination=page_town)
+page_hunt.link(button=G.I_BACK_YOLLOW, destination=page_town)
 page_town.link(button=G.I_TOWN_GOTO_HUNT, destination=page_hunt)
 # 协同斗技 draft_duel
 page_draft_duel = Page(G.I_CHECK_DRAFT_DUEL)
