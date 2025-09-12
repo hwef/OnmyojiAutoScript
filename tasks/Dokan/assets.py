ce5ecec21fbeea4e85da0c1862096962a1e0bba5
ljxun <591692130@qq.com>
2025-09-12 22:01:42 +0800

    feat(图像识别): 更新准备和进入战斗的图像识别区域
    - 修改了 GeneralBattle 和 Dokan任务中准备和进入战斗状态的图像识别区域
    - 调整了图像识别的 ROI 区域，以提高识别准确率- 更新了 assets.py 和 image.json 文件中的相应配置

--- tasks/Dokan/assets.py
+++ tasks/Dokan/assets.py
@@ -97,7 +97,7 @@ class DokanAssets:
 	# 状态：达到失败次数，CD中。观战按钮 
 	I_RYOU_DOKAN_CD = RuleImage(roi_front=(1122,566,117,74), roi_back=(1122,566,117,74), threshold=0.8, method="Template matching", file="./tasks/Dokan/res/res_ryou_dokan_cd.png")
 	# 状态：进入战斗，待开始，右下角图标。TODO 欠截图。 
-	I_RYOU_DOKAN_IN_FIELD = RuleImage(roi_front=(1128,536,100,100), roi_back=(1128,536,100,100), threshold=0.8, method="Template matching", file="./tasks/Dokan/res/res_ryou_dokan_in_field.png")
+	I_RYOU_DOKAN_IN_FIELD = RuleImage(roi_front=(1128,536,100,100), roi_back=(1032,492,239,210), threshold=0.8, method="Template matching", file="./tasks/Dokan/res/res_ryou_dokan_in_field.png")
 	# 馆主战等待中 
 	I_DOKAN_BOSS_WAITING = RuleImage(roi_front=(542,142,195,45), roi_back=(542,142,195,45), threshold=0.8, method="Template matching", file="./tasks/Dokan/res/res_dokan_boss_waiting.png")
 	# 打馆主胜利 
