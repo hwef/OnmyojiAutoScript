25b8e57b45af94ba5c162c62d38c3f041e1c105b
ljxun <591692130@qq.com>
2025-10-17 20:43:53 +0800

    feat(ActivityCommon2): 实现活动通用脚本任务模块
    - 新增 ActivityCommon2 脚本任务类，继承自 ActivityCommonScriptTask
    - 引入 Battle、Delegate 和 General 子任务模块并初始化
    -重构 run 方法以支持新的挑战和战斗资源配置路径- 新增 general.py 文件实现通用活动逻辑- 集成切换御魂、进入挑战界面和开始战斗功能
    - 添加限制时间和次数控制机制
    - 支持奖励领取和异常点击检测
    - 提供战斗状态监控及推送通知功能
    - 增加自动跳转至清理御魂任务的功能
    - 优化图像模板匹配和点击策略逻辑

--- tasks/ActivityCommon2/script_task.py
+++ tasks/ActivityCommon2/script_task.py
@@ -1,27 +1,33 @@
 # This Python file uses the following encoding: utf-8
 # @author runhey
 # github https://github.com/runhey
-
+from tasks.ActivityCommon.challenge import ScriptTask as Battle
+from tasks.ActivityCommon.delegate import ScriptTask as Delegate
+from tasks.ActivityCommon.general import ScriptTask as General
 from tasks.ActivityCommon.script_task import ScriptTask as ActivityCommonScriptTask
 """ 活动通用2 """
 
 
 class ScriptTask(ActivityCommonScriptTask):
+    def __init__(self, config):
+        super().__init__(config)
+        self.Battle = Battle(self.config)
+        self.General = General(self.config)
+        self.Delegate = Delegate(self.config)
 
     def run(self):
         config = self.config.activity_common_2
         # 加载所有图片
-        goto_challenge_folder = "ActivityCommon2/gotoActivity"
-        battle_folder = "ActivityCommon/战斗"
-        self.run_config(config, goto_challenge_folder, battle_folder)
+        goto_challenge_folder = "./tasks/ActivityCommon2/gotoChallenge"
+        battle_folder = "./tasks/ActivityCommon/战斗"
+
+        self.General.run_config(config, goto_challenge_folder, battle_folder)
 
 
 if __name__ == '__main__':
     from module.config.config import Config
-    from module.device.device import Device
 
-    c = Config('switch')
-    d = Device(c)
-    t = ScriptTask(c, d)
+    c = Config('4399')
+    t = ScriptTask(c)
 
     t.run()
