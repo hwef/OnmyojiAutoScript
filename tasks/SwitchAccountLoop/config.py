7aeb3816b62d74be61ee487be2cda42a5981773f
ljxun <591692130@qq.com>
2025-10-19 23:39:29 +0800

    fix(SwitchAccountLoop): 调整任务默认时间范围
    - 将任务开始时间从 06:00:00 修改为 00:00:00
    - 将任务结束时间从 23:00:00 修改为 23:59:59

--- tasks/SwitchAccountLoop/config.py
+++ tasks/SwitchAccountLoop/config.py
@@ -6,8 +6,8 @@ from tasks.Component.config_base import ConfigBase, Time
 
 class LoopConfig(BaseModel):
     accounts_file: str = Field(default='', description='需要切换账号的文件名：比如（accounts.json）')
-    task_start_time: Time = Field(default=Time(6, 0, 0), description='任务开始时间')
-    task_end_time: Time = Field(default=Time(23, 0, 0), description='任务结束时间')
+    task_start_time: Time = Field(default=Time(0, 0, 0), description='任务开始时间')
+    task_end_time: Time = Field(default=Time(23, 59, 59), description='任务结束时间')
     task_interval: Time = Field(default=Time(1, 0, 0), description='间隔时间')
 
 
