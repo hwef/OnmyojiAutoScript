40340ef4851b08b54da0f3447935e07795f10577
ljxun <591692130@qq.com>
2025-10-22 12:38:01 +0800

    fix(logger):修正了 flutter_formatter 中的消息格式化字符串
    - 修正了 flutter_formatter 中的消息格式化字符串
    - 统一了日志时间戳的显示格式
    - 确保不同日志输出的一致性

--- module/logger.py
+++ module/logger.py
@@ -62,7 +62,7 @@ file_formatter = logging.Formatter(
 console_formatter = logging.Formatter(
     fmt='%(asctime)s.%(msecs)03d │ %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 flutter_formatter = logging.Formatter(
-    fmt='%(levelname)1s | %(asctime)s.%(msecs)03d | %(message)08s', datefmt='%H:%M:%S')
+    fmt='%(levelname)1s | %(asctime)s.%(msecs)03d | %(message)s', datefmt='%H:%M:%S')
 
 # ======================================================================================================================
 #            Set console logger
