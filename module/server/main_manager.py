269d1173a2db54a5da5a10fb469ac6e0390ec747
ljxun <591692130@qq.com>
2025-10-26 19:30:11 +0800

    refactor(server): 移除OCR服务器相关逻辑
    - 删除了OCR服务器进程停止的调用
    - 移除了与部署配置相关的OCR服务器状态检查
    - 清理了不再使用的导入模块和状态管理代码
    - 简化了服务器关闭时的清理流程
    - 去除了冗余的日志注释行
    - 优化了脚本进程停止的异步等待逻辑

--- module/server/main_manager.py
+++ module/server/main_manager.py
@@ -10,10 +10,8 @@ import signal
 from asyncio.tasks import Task
 from module.config.config import Config
 from module.logger import logger
-from module.ocr.rpc import stop_ocr_server_process
 from module.server.config_manager import ConfigManager
 from module.server.script_process import ScriptProcess, ScriptState
-from module.server.setting import State
 from threading import Thread
 
 
@@ -77,8 +75,6 @@ class MainManager(ConfigManager):
                 logger.info('Kill all server')
                 for script_p in self.script_process.values():
                     await script_p.stop()
-                if State.deploy_config and State.deploy_config.UseOcrServer:
-                    stop_ocr_server_process()
                 logger.info('Kill push data thread')
                 sys.exit(0)
             # logger.info(asyncio.all_tasks())
