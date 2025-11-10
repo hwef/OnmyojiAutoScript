bc924263872148dfb151e646103ba8eacf6038cd
ljxun <591692130@qq.com>
2025-11-10 19:17:55 +0800

    fix(SwitchAccount): 处理4399登录活动弹窗问题
    - 在登录表单中增加对4399活动弹窗的关闭逻辑
    - 寻找角色时也增加对4399活动弹窗的关闭逻辑
    - 调整import顺序并移除无用导入
    - 更新默认配置为4399平台
    - 修正switchAccount调用方式一致性

--- tasks/Component/SwitchAccount/switch_account.py
+++ tasks/Component/SwitchAccount/switch_account.py
@@ -56,7 +56,7 @@ class SwitchAccount(LoginAccount, ExitGame, GameUi, SwitchAccountAssets):
 
 
 if __name__ == '__main__':
-    config = Config('oas1')
+    config = Config('4399')
     device = Device(config)
     account_list = [
         # AccountInfo(account="178****7164", account_alias="178****7164", apple_or_android=True, character="浙沥沥、下雨", svr="全球国际区"),
